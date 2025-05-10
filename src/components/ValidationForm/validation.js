c059a11b2dca6fff468ff4223e3ba5aef7bbcba6
Supreet-Pradhan_valtech <supreet.pradhan@valtech.com>
2025-05-10 22:16:02 +0530

    Added password validation

--- src/components/ValidationForm/validation.js
+++ src/components/ValidationForm/validation.js
@@ -1,126 +1,156 @@
-import React, { useState } from "react"
-import { EmailSpan, EyeIconSpan, FormContainer, FormInput, FormLabel, PasswordChecker, SubmitButton, ConfirmSpan,PasswordStrengthSpan, PasswordStrengthContent, ValidSpan } from "./validation.style"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEye, faEyeSlash} from "@fortawesome/free-regular-svg-icons"
-import { faCircleXmark, faCircleCheck } from "@fortawesome/free-solid-svg-icons"
-import ListArray from "./ListArray.js"
+import React, { useState } from "react";
+import {
+  EmailSpan,
+  EyeIconSpan,
+  FormContainer,
+  FormInput,
+  FormLabel,
+  PasswordChecker,
+  SubmitButton,
+  ConfirmSpan,
+  PasswordStrengthSpan,
+  PasswordStrengthContent,
+  ValidSpan,
+  InputSpanWrapper,
+} from "./validation.style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCircleXmark,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import ListArray from "./ListArray.js";
 // import PasswordStrengthBar from 'react-password-strength-bar'
 
 export const FormValidation = () => {
-  const [newPass, setNewPass] = useState(true) // Icon Toggle
-  const [confirmPass, setConfirmPass] = useState(true) // Icon Toggle
-  const [emailValidation, setEmailVaidation] = useState(false)
-  const [validPassword,setValidPassword] = useState({
-    validNewPass : false,
-    validConfirmPass : false
-  })
+  const [newPass, setNewPass] = useState(true); // Icon Toggle
+  const [confirmPass, setConfirmPass] = useState(true); // Icon Toggle
+  const [emailValidation, setEmailVaidation] = useState(false);
+  const [validPassword, setValidPassword] = useState({
+    validNewPass: false,
+    validConfirmPass: false,
+  });
   const [inputVal, setInputVal] = useState({
-    email : '',
-    newPass : '',
-    confirmPass : ''
-  })
+    email: "",
+    newPass: "",
+    confirmPass: "",
+  });
   const [newPassCheck, setNewPassCheck] = useState({
-    length : false,
-    lowercase : false,
-    uppercase : false,
-    numeric : false,
-    specialCharacter : false,
-  })
+    length: false,
+    lowercase: false,
+    uppercase: false,
+    numeric: false,
+    specialCharacter: false,
+  });
 
   const PassToggle = () => {
-    setNewPass(!newPass)
-  }
+    setNewPass(!newPass);
+  };
   const ConfirmPassToggle = () => {
-    setConfirmPass(!confirmPass)
-  }
-  
-  const handleInputs = (e) => {
-    const {name,value} = e.target
-    // console.log(name,value)
-    if(value !== ' '){
-      setInputVal(prevInputValue => {
-        const updatedInputVal = {
-        ...prevInputValue,
-        [name] : value
-        }  
-        passwordChecker(updatedInputVal)
-        confirmPassChecker(updatedInputVal)
-        if(name === 'email'){
-          emailChecker(name,value)
-        }
-        return updatedInputVal
-      })
-    }
-  }
+    setConfirmPass(!confirmPass);
+  };
 
-  const emailChecker = (name,value) => {
-    if(name === 'email' && validateEmail(value)){
-      setEmailVaidation(true)
-    }else{
-      setEmailVaidation(false)
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
+    // console.log(name,value)
+    if (value !== " ") {
+      setInputVal((prevInputValue) => {
+        const updatedInputVal = {
+          ...prevInputValue,
+          [name]: value,
+        };
+        passwordChecker(updatedInputVal);
+        confirmPassChecker(updatedInputVal);
+        if (name === "email") {
+          emailChecker(name, value);
+        }
+        return updatedInputVal;
+      });
     }
-  }
+  };
+
+  const emailChecker = (name, value) => {
+    if (name === "email" && validateEmail(value)) {
+      setEmailVaidation(true);
+    } else {
+      setEmailVaidation(false);
+    }
+  };
 
   const validateEmail = (email) => {
-    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-    return regex.test(email)
-  }
+    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+  };
 
   const confirmPassChecker = (updatedInputVal) => {
-    const newPassValue = updatedInputVal.newPass
-    const newConfirmPassValue = updatedInputVal.confirmPass
-    if (newPassValue === newConfirmPassValue && newPassValue !== '' && newConfirmPassValue !== ''){
-      setValidPassword(prev => ({...prev, validConfirmPass : true}))
+    const newPassValue = updatedInputVal.newPass;
+    const newConfirmPassValue = updatedInputVal.confirmPass;
+    if (
+      newPassValue === newConfirmPassValue &&
+      newPassValue !== "" &&
+      newConfirmPassValue !== ""
+    ) {
+      setValidPassword((prev) => ({ ...prev, validConfirmPass: true }));
+    } else {
+      setValidPassword((prev) => ({ ...prev, validConfirmPass: false }));
     }
-    else{
-      setValidPassword(prev => ({...prev, validConfirmPass : false}))
-    }
-  }
+  };
 
   const passwordChecker = (updatedInputVal) => {
-    const newPassValue = updatedInputVal.newPass
+    const newPassValue = updatedInputVal.newPass;
 
-    setNewPassCheck(prevState => {
+    setNewPassCheck((prevState) => {
       const updatedNewPassCheck = {
         ...prevState,
         length: newPassValue.length >= 10,
         lowercase: /[a-z]/.test(newPassValue),
         uppercase: /[A-Z]/.test(newPassValue),
         numeric: /[0-9]/.test(newPassValue),
-        specialCharacter: /[!@#$%&*_+-]/.test(newPassValue)
-      }
+        specialCharacter: /[!@#$%&*_+-]/.test(newPassValue),
+      };
 
-      const allConditionsMet = Object.values(updatedNewPassCheck).every(condition => condition)
-      setValidPassword(prevState => ({...prevState, validNewPass: allConditionsMet}))
-      return updatedNewPassCheck
-    })
-
-  }
+      const allConditionsMet = Object.values(updatedNewPassCheck).every(
+        (condition) => condition
+      );
+      setValidPassword((prevState) => ({
+        ...prevState,
+        validNewPass: allConditionsMet,
+      }));
+      return updatedNewPassCheck;
+    });
+  };
 
   const passwordStrength = () => {
-    const { length, lowercase, uppercase, numeric, specialCharacter } = newPassCheck
-    const conditionsMet = [length, lowercase, uppercase, numeric, specialCharacter].filter(condition => condition).length
-  
-    switch(true){
-      case conditionsMet === 5 :
-        return 'Strong'
+    const { length, lowercase, uppercase, numeric, specialCharacter } =
+      newPassCheck;
+    const conditionsMet = [
+      length,
+      lowercase,
+      uppercase,
+      numeric,
+      specialCharacter,
+    ].filter((condition) => condition).length;
+
+    switch (true) {
+      case conditionsMet === 5:
+        return "Strong";
       case conditionsMet >= 4:
-        return 'Medium'
-      case conditionsMet >= 2 :
-        return 'Good'
-      case conditionsMet > 0 :
-        return 'Weak'
-      default :
-        return ''
+        return "Medium";
+      case conditionsMet >= 2:
+        return "Good";
+      case conditionsMet > 0:
+        return "Weak";
+      default:
+        return "";
     }
-  }
+  };
   const handleKeyDown = (e, nextInput) => {
-    if(e.key === 'Enter' || 'ArrowDown'){
+    if (e.key === "Enter" || "ArrowDown") {
       // console.log(e.key)
       // e.preventDefault()
-      nextInput.focus()
+      nextInput.focus();
     }
-  }
+  };
   return (
     <main className="form-container">
       <h1>Form Validation</h1>
@@ -137,118 +167,142 @@ export const FormValidation = () => {
               onChange={handleInputs}
               autoFocus
               onDragStart={(e) => e.preventDefault()}
-              onKeyDown={(e) => handleKeyDown(e, document.getElementById("newPass"))}
+              onKeyDown={(e) =>
+                handleKeyDown(e, document.getElementById("newPass"))
+              }
             />
             {inputVal?.email.length > 0 && (
               <EmailSpan $emailvalidation={emailValidation}>
                 {emailValidation ? (
                   <>
-                   <FontAwesomeIcon icon={faCircleCheck}/> {inputVal.email}
+                    <FontAwesomeIcon icon={faCircleCheck} /> {inputVal.email}
                   </>
-                  ) : (     
-                    <span>Invalid Email!!</span>            
+                ) : (
+                  <span>Invalid Email!!</span>
                 )}
               </EmailSpan>
             )}
           </div>
           <div className="passwordField">
             <FormLabel htmlFor="newPass">Password</FormLabel>
-            <FormInput
-              type={newPass? 'password' : 'text'}
-              id="newPass"
-              name="newPass"
-              placeholder="New Password"
-              value={inputVal.newPass}
-              onChange={handleInputs}
-              onPaste={(e) => {
-                e.preventDefault()
-                return false
-              }}
-              onDragStart={(e) => e.preventDefault()}
-              disabled ={!emailValidation}
-              onKeyDown={(e) => handleKeyDown(e, document.getElementById("confirmPass"))}
-            />
-            <div className="inputSpan">
-              <EyeIconSpan onClick={PassToggle} $validPass={validPassword.validNewPass}>
+            <InputSpanWrapper className="inputSpan">
+              <FormInput
+                type={newPass ? "password" : "text"}
+                id="newPass"
+                name="newPass"
+                placeholder="New Password"
+                value={inputVal.newPass}
+                onChange={handleInputs}
+                onPaste={(e) => {
+                  e.preventDefault();
+                  return false;
+                }}
+                onDragStart={(e) => e.preventDefault()}
+                disabled={!emailValidation}
+                onKeyDown={(e) =>
+                  handleKeyDown(e, document.getElementById("confirmPass"))
+                }
+              />
+              <EyeIconSpan
+                onClick={PassToggle}
+                $validPass={validPassword.validNewPass}
+              >
                 {newPass ? (
                   <FontAwesomeIcon icon={faEyeSlash} size="lg" />
-                  ) : (
-                    <FontAwesomeIcon icon={faEye} size="lg" />
-                )}
-              </EyeIconSpan>
-              {validPassword.validNewPass ? 
-                <ValidSpan $validPass={validPassword.validNewPass}>
-                  {validPassword.validNewPass ? (
-                      <FontAwesomeIcon icon={faCircleCheck} size="lg" />
-                    ) : (
-                      <FontAwesomeIcon icon={faCircleXmark} size="lg" />
-                    )
-                  }
-                </ValidSpan>    
-              : null       
-              }
-            </div>
-          </div>
-          <div className="confirmPassField">
-            <FormLabel htmlFor="confirmPass">Confirm Password</FormLabel>
-            <FormInput
-              type={confirmPass? 'password' : 'text'}
-              id="confirmPass"
-              name="confirmPass"
-              placeholder="Confirm Your Password"
-              value={inputVal.confirmPass}
-              onChange={handleInputs}
-              onPaste={(e) => {
-                e.preventDefault()
-                return false
-              }}
-              onDragStart={(e) => e.preventDefault()}
-              disabled ={!validPassword.validNewPass}
-              onKeyDown={(e) => handleKeyDown(e, document.getElementById("submitButton"))}
-            />
-            <div className="inputSpan">
-              <EyeIconSpan onClick={ConfirmPassToggle} $validPass={validPassword.validConfirmPass}>
-                {confirmPass ? (
-                  <FontAwesomeIcon icon={faEyeSlash} size="lg" />
-                  ) : (
+                ) : (
                   <FontAwesomeIcon icon={faEye} size="lg" />
                 )}
               </EyeIconSpan>
-              {validPassword.validConfirmPass ?               
+              {validPassword.validNewPass ? (
+                <ValidSpan $validPass={validPassword.validNewPass}>
+                  {validPassword.validNewPass ? (
+                    <FontAwesomeIcon icon={faCircleCheck} size="lg" />
+                  ) : (
+                    <FontAwesomeIcon icon={faCircleXmark} size="lg" />
+                  )}
+                </ValidSpan>
+              ) : null}
+            </InputSpanWrapper>
+          </div>
+          <div className="confirmPassField">
+            <FormLabel htmlFor="confirmPass">Confirm Password</FormLabel>
+            <InputSpanWrapper className="inputSpan">
+              <FormInput
+                type={confirmPass ? "password" : "text"}
+                id="confirmPass"
+                name="confirmPass"
+                placeholder="Confirm Your Password"
+                value={inputVal.confirmPass}
+                onChange={handleInputs}
+                onPaste={(e) => {
+                  e.preventDefault();
+                  return false;
+                }}
+                onDragStart={(e) => e.preventDefault()}
+                disabled={!validPassword.validNewPass}
+                onKeyDown={(e) =>
+                  handleKeyDown(e, document.getElementById("submitButton"))
+                }
+              />
+              <EyeIconSpan
+                onClick={ConfirmPassToggle}
+                $validPass={validPassword.validConfirmPass}
+              >
+                {confirmPass ? (
+                  <FontAwesomeIcon icon={faEyeSlash} size="lg" />
+                ) : (
+                  <FontAwesomeIcon icon={faEye} size="lg" />
+                )}
+              </EyeIconSpan>
+              {validPassword.validConfirmPass ? (
                 <ValidSpan $validPass={validPassword.validConfirmPass}>
                   {validPassword.validConfirmPass ? (
                     <FontAwesomeIcon icon={faCircleCheck} size="lg" />
-                    ) : (
-                      <FontAwesomeIcon icon={faCircleXmark} size="lg" />
-                    )
-                  }
+                  ) : (
+                    <FontAwesomeIcon icon={faCircleXmark} size="lg" />
+                  )}
                 </ValidSpan>
-              : null
-              }
-            </div>
+              ) : null}
+            </InputSpanWrapper>
             {inputVal.confirmPass.length > 0 && (
               <ConfirmSpan $matchedpassword={validPassword.validConfirmPass}>
                 {validPassword.validConfirmPass ? (
-                    <span>Password Matched!!</span>
-                  ) : (     
-                    <span>Passwords do not match!!</span>           
+                  <span>Password Matched!!</span>
+                ) : (
+                  <span>Passwords do not match!!</span>
                 )}
               </ConfirmSpan>
             )}
           </div>
           <div className="buttonField">
-            <SubmitButton id="submitButton" disabled={!validPassword.validNewPass || !validPassword.validConfirmPass || !emailValidation}>Submit</SubmitButton>
+            <SubmitButton
+              id="submitButton"
+              disabled={
+                !validPassword.validNewPass ||
+                !validPassword.validConfirmPass ||
+                !emailValidation
+              }
+            >
+              Submit
+            </SubmitButton>
           </div>
-          <PasswordStrengthSpan $strength={passwordStrength(newPassCheck)}></PasswordStrengthSpan>
-          <PasswordStrengthContent>{passwordStrength(newPassCheck)}</PasswordStrengthContent>
+          <PasswordStrengthSpan
+            $strength={passwordStrength(newPassCheck)}
+          ></PasswordStrengthSpan>
+          <PasswordStrengthContent>
+            {passwordStrength(newPassCheck)}
+          </PasswordStrengthContent>
         </form>
-      <PasswordChecker>
-        <h3>Password must include:</h3>
-        <ul>
-          <ListArray inputVal={inputVal} newPassCheck={newPassCheck}></ListArray>
-        </ul>
-      </PasswordChecker>          
-      </FormContainer>      
+        <PasswordChecker>
+          <h3>Password must include:</h3>
+          <ul>
+            <ListArray
+              inputVal={inputVal}
+              newPassCheck={newPassCheck}
+            ></ListArray>
+          </ul>
+        </PasswordChecker>
+      </FormContainer>
     </main>
-  )
-}
+  );
+};
--- src/components/ValidationForm/validation.style.js
+++ src/components/ValidationForm/validation.style.js
@@ -1,28 +1,33 @@
 import styled from 'styled-components'
 
 export const FormContainer = styled.article`
-  width : 55%;
+  max-width : 520px;
+  width: 100%;
   margin : 0 auto;
   background-color : var(--background);
   padding : 1% 3%;
 `
 export const FormLabel = styled.label`
-  width : 80%;
+  width : 100%;
   margin-top : 20px;
   display : inline-block;
 `
 export const FormInput = styled.input`
-  width : 80%;
+  width : 100%;
   display : block;
   padding : 8px 0;
 `
+export const InputSpanWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`
 
 export const SubmitButton = styled.button`
-  background-color : ${props => props.disabled === true ? 'var(--disabled)' : 'var(--btn)'};
+  background-color : ${props => props.disabled ? 'var(--disabled)' : 'var(--btn)'};
   border : none;
   padding : 12px 0px;
   color : #fff;
-  width : 80%;
+  width : 100%;
   cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
   &:hover {
     background-color: ${props => !props.disabled && 'var(--btn-hover)'};
@@ -35,31 +40,32 @@ export const PasswordChecker = styled.article`
 export const EmailSpan  = styled.span`
   display : inline-block;
   margin-top : 3px;
-  color : ${props => props.$emailvalidation === true ? 'var(--success)' : 'var(--error)' };
+  color : ${props => props.$emailvalidation ? 'var(--success)' : 'var(--error)' };
 `
 
 export const EyeIconSpan = styled.span`
   position : absolute;
   bottom : 8px;
-  left: ${props => props.$validPass ? '505px' : '530px'};
+  right: 3px;
+  cursor: pointer;
 `
 export const List = styled.li`
  ${props => props.$liststyle ? '' : 'list-style : none;'};
-  color : ${props => props.$liststyle? '' : props.$passcheck === true ? 'var(--success)' : 'var(--error)'};
+  color : ${props => props.$liststyle? '' : props.$passcheck ? 'var(--success)' : 'var(--error)'};
 `
 export const ConfirmSpan = styled.span`
   display : inline-block;
   margin-top : 3px;
-  color : ${props => props.$matchedpassword === true ? 'var(--success)' : 'var(--error)' };
+  color : ${props => props.$matchedpassword ? 'var(--success)' : 'var(--error)' };
 `
 export const PasswordStrengthContent = styled.span`
   display: block;
   margin-top: 2px;
 `
 export const PasswordStrengthSpan = styled.span`
-  display: inline-block;
+  /* display: inline-block; */
   margin-top: 5px;
-  width : 80%;
+  width : 100%;
   height: 5px;
   display : ${props => props.$strength ? 'block' : 'none'};
   background-color: ${props => {
@@ -81,7 +87,7 @@ export const PasswordStrengthSpan = styled.span`
 export const ValidSpan = styled.span`
   position : absolute;
   bottom : 8px;
-  left : 535px;
-  color : ${props => props.$validPass === true ? 'var(--success)' : 'var(--error)' };
+  right: 35px;
+  color : ${props => props.$validPass ? 'var(--success)' : 'var(--error)' };
   display: ${props => props.$validPass ? 'block' : 'none'};
 `
